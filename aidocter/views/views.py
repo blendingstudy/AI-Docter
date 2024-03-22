d585f012a939046354638f10260659fcd1a9c37d
ykh9759 <ykh059@naver.com>
2024-03-22 13:52:54 +0900

    '알림창추가및세션으로대화내역관리'

--- aidocter/views/views.py
+++ aidocter/views/views.py
@@ -8,7 +8,25 @@ def index(request):
 
 #화면 호출 메소드
 def view(request, view_name):
-    return render(request, f'{view_name}.html')
+    
+    context: dict = {}
+    
+    if request.method == 'GET':
+    
+        message = request.GET.get('message')
+
+        if message:
+            context['message'] = message
+            
+        if view_name == 'chat':
+            id = request.session.get('id', None)
+            if not id:
+                message = '로그인정보 없음'
+                # 회원가입이 성공했을 경우 로그인 페이지로 리다이렉트
+                return redirect(f'/view/login?message={message}') 
+                
+    
+    return render(request, f'{view_name}.html', context)
 
 #회원가입
 def register(request):
@@ -22,13 +40,15 @@ def register(request):
             member = Member.objects.get(id=id)
             print("회원존재: "+str(member))
             # 이미 존재하는 아이디라면 실패 메시지를 표시하고 이전 페이지로 리다이렉트
-            return redirect(request.META.get('HTTP_REFERER', '/'))
+            message = '회원존재: 아이디중복'
+            return redirect(f'/view/register?message={message}') 
         except Member.DoesNotExist:
             # 존재하지 않는 아이디라면 새로운 멤버 생성
             new_member = Member.objects.create(id=id, pw=pw, name=name)
             print("회원생성: ", new_member)
+            message = '회원가입 완료'
             # 회원가입이 성공했을 경우 로그인 페이지로 리다이렉트
-            return redirect('login')  # login은 로그인 페이지의 URL 이름입니다.
+            return redirect(f'/view/login?message={message}')  # login은 로그인 페이지의 URL 이름입니다.
     
     print("회원생성 실패")
     return render(request, 'login.html')
@@ -36,6 +56,27 @@ def register(request):
 #로그인
 def login(request):
     
-    return render(request, 'chat.html')
+    if request.method == 'POST':
+        id = request.POST.get('id')
+        pw = request.POST.get('pw')
+        
+        try:
+            # 특정 id를 가진 멤버 조회
+            member = Member.objects.get(id=id, pw=pw)
+            print("로그인성공: "+str(member))
+            
+            request.session['id'] = id
+            
+            # 이미 존재하는 아이디라면 실패 메시지를 표시하고 이전 페이지로 리다이렉트
+            return redirect('/view/chat') 
+        except Member.DoesNotExist:
+            print("로그인실패: 회원없음")
+            
+            message = '로그인실패: 아이디 또는 비밀번호 확인'
+            # 로그인 실패 메시지를 표시하고 이전 페이지로 리다이렉트
+            return redirect(f'/view/login?message={message}') 
+    
+    print("로그인 실패")
+    return redirect('/view/login') 
     
 
--- api/views/chat_views.py
+++ api/views/chat_views.py
@@ -1,11 +1,9 @@
 import os
-from django.http import HttpResponse, JsonResponse
-from dotenv import load_dotenv
-from django.shortcuts import render
+from django.http import JsonResponse
 from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
 from langchain.chains.llm import LLMChain
-from langchain.prompts.chat import ChatPromptTemplate, AIMessagePromptTemplate, HumanMessagePromptTemplate, SystemMessagePromptTemplate
+from langchain.prompts.chat import ChatPromptTemplate, HumanMessagePromptTemplate, SystemMessagePromptTemplate
 from langchain.memory import ConversationBufferMemory
 
 load_dotenv()
@@ -13,15 +11,19 @@ load_dotenv()
 OPEN_API_KEY = os.getenv('OPEN_API_KEY')
 
 #대화내용 저장 메모리
-memory = ConversationBufferMemory(memory_key="chat_history")
+memorys: dict = {}
     
 #LLM채팅 요청 API
 def chat_llm(request):
+    id = request.session.get('id', None)
     llm = ChatOpenAI(openai_api_key=OPEN_API_KEY)
     context = {}
 
     message = request.GET.get('message')  # 사용자가 제공한 메시지 가져오기
     context['user_message'] = message  # 사용자가 제공한 메시지를 context에 추가
+    
+    if not id in memorys:
+        memorys[id] = ConversationBufferMemory(memory_key="chat_history")
 
     if message:  # 사용자가 메시지를 제공한 경우에만 처리
         prompt = ChatPromptTemplate.from_messages(
@@ -39,7 +41,7 @@ def chat_llm(request):
             llm=llm, 
             prompt=prompt,
             verbose=True,
-            memory=memory
+            memory=memorys[id]
         )
         
         context['llm_message'] = response.predict(input=message)  # 언어 처리 모델로 메시지 처리 후 결과를 context에 추가
