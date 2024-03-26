b6bc87630d4359b732bb57ba1b73f6750b63c6a6
ykh9759 <ykh059@naver.com>
2024-03-26 14:36:09 +0900

    '부트스트랩적용'

--- api/views/chat_views.py
+++ api/views/chat_views.py
@@ -3,7 +3,7 @@ from django.http import JsonResponse
 from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
 from langchain.chains.llm import LLMChain
-from langchain.prompts.chat import ChatPromptTemplate, HumanMessagePromptTemplate, SystemMessagePromptTemplate
+from langchain.prompts.chat import ChatPromptTemplate, HumanMessagePromptTemplate, SystemMessagePromptTemplate, MessagesPlaceholder
 from langchain.memory import ConversationBufferMemory
 
 load_dotenv()
@@ -15,36 +15,36 @@ memorys: dict = {}
     
 #LLM채팅 요청 API
 def chat_llm(request):
-    id = request.session.get('id', None)
+    user = request.user
+    print(user)
     llm = ChatOpenAI(openai_api_key=OPEN_API_KEY)
     context = {}
 
     message = request.GET.get('message')  # 사용자가 제공한 메시지 가져오기
     context['user_message'] = message  # 사용자가 제공한 메시지를 context에 추가
     
-    if not id in memorys:
-        memorys[id] = ConversationBufferMemory(memory_key="chat_history")
+    if not user in memorys:
+        memorys[user] = ConversationBufferMemory(memory_key="chat_history")
 
     if message:  # 사용자가 메시지를 제공한 경우에만 처리
-        prompt = ChatPromptTemplate.from_messages(
-            [
-                SystemMessagePromptTemplate.from_template("""
-                    Please answer in Korean
-                    You are an AI doctor counseling patients
-                    {chat_history}
-                """),
-                HumanMessagePromptTemplate.from_template("{input}"),
-            ]
-        )
+        prompt = ChatPromptTemplate.from_messages([
+            SystemMessagePromptTemplate.from_template("""
+                Please answer in Korean
+                You are an AI doctor counseling patients
+                
+                {chat_history}
+            """),
+            HumanMessagePromptTemplate.from_template("{input}"),
+        ])
         
         response = LLMChain(
             llm=llm, 
             prompt=prompt,
             verbose=True,
-            memory=memorys[id]
+            memory=memorys[user]
         )
         
-        context['llm_message'] = response.predict(input=message)  # 언어 처리 모델로 메시지 처리 후 결과를 context에 추가
+        context['llm_message'] = response.run(input=message)  # 언어 처리 모델로 메시지 처리 후 결과를 context에 추가
 
     return JsonResponse(context)
 
